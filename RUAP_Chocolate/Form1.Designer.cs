c27f078addd35f7dbb78fbb2050fe24161c1b5b5
bmatijevic65 <bmatijevic65@gmail.com>
2020-02-16 14:04:33 +0100

    Dodavanje koda

---
+++ RUAP_Chocolate/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RUAP_Chocolate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(242, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Predict";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(33, 59);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(212, 22);
+            this.txtCompany.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Company:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(322, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cocoa Percent:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bean Type:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(326, 77);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(388, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(471, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Trinitario",
+            "Criollo",
+            "Criollo (Porcelana)",
+            "Trinitario & Criollo",
+            "Forastero",
+            "Forastero (Nacional)",
+            "Forastero (Arriba)",
+            "Blend"});
+            this.listBox1.Location = new System.Drawing.Point(56, 158);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 132);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 345);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCompany);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Chocolate Predictions";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ RUAP_Chocolate/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Globalization;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace RUAP_Chocolate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+       public static float ConvertFunc(string strlist )
+        {
+
+            string newString;
+            strlist = strlist.Replace("\"", "");  //micanje dvostrukih navodnika
+            int startIndex = strlist.IndexOf(":") + ":".Length;
+            int endIndex = strlist.IndexOf(",");
+            newString = strlist.Substring(startIndex, endIndex - startIndex);  //izdvajanje predviđenih vjerojatnosi
+            float s1 = float.Parse(newString); //pretvaranje u float vrijednost
+            return s1;
+        }
+
+    private string company;
+        private string cocoaPercent;
+        private string beanType;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            company = txtCompany.Text.ToString();
+
+
+            if (trackBar1.Value <= 99)   
+            {
+
+                cocoaPercent = "0." + trackBar1.Value.ToString();   //podatci iz trackBara
+            }
+
+            else
+            {
+                
+                cocoaPercent = "1.0";
+            }
+
+            beanType = listBox1.GetItemText(listBox1.SelectedItem);  //podatci iz ListBoxa
+
+            
+            using (var client = new HttpClient())   //Request-Response
+            {
+                var scoreRequest = new
+                {
+                    Inputs = new Dictionary<string, List<Dictionary<string, string>>>() {
+                        {
+                            "input1",
+                            new List<Dictionary<string, string>>(){new Dictionary<string, string>(){
+                                            {
+                                                "Company", company.ToString()
+                                            },
+                                            {
+                                                "Cocoa Percent", cocoaPercent.ToString()
+                                            },
+                                            {
+                                                "Bean Type", beanType.ToString()
+                                            },
+                                }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+              
+                const string apiKey = "uCtXmwtCz2IwkYixE/VEbZKM8vFGxFRALAKnZZQSIB2hNcqw7YfN9sdmO88MDPQFek1eeWdUYjK+sctX5Z/ptw=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("https://europewest.services.azureml.net/workspaces/e4a7547db8f74b0e87e7382f6d2ace05/services/a1fcb6303b63488fa85e0c4e6a09be1e/execute?api-version=2.0&format=swagger");
+
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                  
+                  
+                    // Taking a string 
+                    string[] spearator = { "Scored " };   //ekstrahiranje potrebnih podataka iz varijable result
+                    Int32 count = 5;
+                    // using the method 
+                    String[] strlist = result.Split(spearator, count,
+                           StringSplitOptions.RemoveEmptyEntries);
+
+                    float bad = ConvertFunc(strlist[1]);
+                    float good = ConvertFunc(strlist[2]);
+                    float excelent = ConvertFunc(strlist[3]);
+
+
+                    float max = Math.Max(Math.Max(bad, good), excelent);
+                    string temp;
+
+                    if (max == bad)
+                        temp = "BAD";
+                    else if (max == good)
+                        temp = "GOOD";
+                    else
+                        temp = "EXCELENT";
+
+                    MessageBox.Show("It's a " + temp + " chocolate", "Result Prediction", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show("The request failed with status code: {0}" + response.StatusCode, "Error", MessageBoxButtons.OK);
+                }
+            }
+
+        }
+       
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+            if (trackBar1.Value <= 99)   //kod ne radi dobro zbog ovoga
+            {
+                label5.Text = "0." + trackBar1.Value.ToString();
+                
+            }
+
+            else
+            {
+                label5.Text = "1.0";
+         
+            }
+        }
+
+        
+    }
+}
